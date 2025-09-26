0eba2ab1bbceb56dd2610b0d4a6dd60e22376f2b
Mythili <mythili4997@gmail.com>
2025-09-26 21:43:27 +0530

    remove id from ApplcationModel

--- Backend/Models/ApplicationModel.js
+++ Backend/Models/ApplicationModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const ApplicationSchema = new mongoose.Schema({
-  _id: ObjectId,
   jobId: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // reference to User (jobseeker)
   status: {
