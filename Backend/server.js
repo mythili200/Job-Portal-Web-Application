93970be7a46425fc18b9c79ea02dc0f87398a7f4
Mythili <mythili4997@gmail.com>
2025-09-26 22:08:41 +0530

    connect mongodb with backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+
 const PORT = 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, (req, res) => {
   console.log(`Sever is running on ${PORT}`);
