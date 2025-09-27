9e4d23cb5ca8ebea22740a35baffd351798ef707
Mythili <mythili4997@gmail.com>
2025-09-27 11:20:15 +0530

    add Middlware to the express application

---
+++ Backend/Middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../Models/UserModel");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "No Token provided" });
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    if (!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res
+      .status(401)
+      .json({ success: false, message: "Unauthorized - Invalid Token" });
+  }
+};
+module.exports = authMiddleware;
