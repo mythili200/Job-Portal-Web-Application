e1b25cb1bbfd3f0aa36b3b437178a995dea7a60c
Mythili <mythili4997@gmail.com>
2025-09-27 14:10:16 +0530

    added cors package add routes

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
+const UserRoutes = require("./Routes/UserRoutes");
+const JobRoutes = require("./Routes/JobRoutes");
+const ApplicationRoutes = require("./Routes/ApplicationRoutes");
+const cors = require("cors");
 
 const PORT = 5000;
-
+app.use(express.json());
+app.use(cors());
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("MongoDB Connected"))
@@ -14,3 +19,6 @@ mongoose
 app.listen(PORT, () => {
   console.log(`Sever is running on ${PORT}`);
 });
+app.use("/api/users", UserRoutes);
+app.use("/api/jobs", JobRoutes);
+app.use("/api/applicaion", ApplicationRoutes);
